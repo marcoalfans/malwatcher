029bfdf188ffad6dbbd7efc0056f13b28e815f48
Marco Alfan <marcoalfans@gmail.com>
2025-06-14 15:09:56 +0700

    update static to dynamic page to show the logs

--- app/scan-result/page.tsx
+++ app/scan-result/page.tsx
@@ -1,20 +1,23 @@
 // app/scan-result/page.ts
-
 import fs from 'fs'
 import path from 'path'
 import { ResultPageClient } from '@/components/result-page-client'
 
+export const dynamic = "force-dynamic"; // ⬅️ ini penting
+
 export default function ScanResultPage() {
-  const logDir = path.join(process.cwd(), 'logs')
-  const files = fs.readdirSync(logDir).filter(f => f.endsWith('.json'))
+  const logDir = path.resolve('./logs');
+  const files = fs.readdirSync(logDir).filter(f => f.endsWith('.json'));
 
   const results = files.map((filename) => {
-    const filePath = path.join(logDir, filename)
-    const data = JSON.parse(fs.readFileSync(filePath, 'utf-8'))
-    return { filename, data }
-  })
+    const filePath = path.join(logDir, filename);
+    const data = JSON.parse(fs.readFileSync(filePath, 'utf-8'));
+    return { filename, data };
+  });
 
-  return (
-    <ResultPageClient results={results}/>
-  )
+  if (!results.length) {
+    return <div className="p-10 text-center text-muted-foreground">⚠️ No logs found in /logs directory.</div>
+  }
+
+  return <ResultPageClient results={results} />;
 }
